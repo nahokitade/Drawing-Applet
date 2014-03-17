be65933d888bac27661346c827eb929a2299d3e7
Naho Kitade <naho.kitade.16@dartmouth.edu>
2014-03-17 16:42:03 -0400

    initial commit

---
+++ rails_projects/naho_homepage/app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def about
+  end
+
+  def projects
+  end
+
+  def resume
+  end
+end
---
+++ rails_projects/naho_homepage/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NahoHomepage::Application.config.session_store :cookie_store, key: '_naho_homepage_session'
